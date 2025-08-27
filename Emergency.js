aa73d18a2f36178069db6db24488d1da69dc00a7
Elias729 <eliashossainrafe72@gmail.com>
2025-08-27 13:19:11 +0600

    added an clicking on the heart icon of any card will increase the count in the navber

--- Emergency.js
+++ Emergency.js
@@ -1,6 +1,18 @@
  const menuToggle = document.getElementById("menuToggle");
  const menuItems = document.getElementById("menuItems");
- 
+
  menuToggle.addEventListener("click", () => {
      menuItems.classList.toggle("hidden")
  });
+
+ const heartBtn = document.getElementById("heartBtn");
+ let heartBtnValue = parseInt(heartBtn.innerText);
+ const likeCount = document.getElementsByClassName("likeCount");
+
+
+ for (let i = 0; i < likeCount.length; i++) {
+     likeCount[i].addEventListener("click", () => {
+         heartBtnValue++;
+         heartBtn.innerText = heartBtnValue;
+     });
+ }
